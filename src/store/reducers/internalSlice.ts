625b39dced437fcb15c2e72ec140e4c5ec900a78
MrsLecter <MissisLecter@protonmail.com>
2023-07-09 18:29:56 +0300

    fix: delete comments

--- src/store/reducers/internalSlice.ts
+++ src/store/reducers/internalSlice.ts
@@ -6,19 +6,7 @@ interface IInternalData {
   firstMessage: boolean;
   isHaveNewQuestion: string;
   userQuestionType: TUserQuestion;
-  // showCommonModal: boolean;
-  // showMenuModal: boolean;
-  // showLoginModal: boolean;
-  // showSignupModal: boolean;
-  // showAboutModal: boolean;
-  // showRestorePasswordModal: boolean;
-  // showPaywallModal: boolean;
-  // showPaywallCardpayModal: boolean;
-  // showPaywallPricingModal: boolean;
-  // showPaywallResultModal: boolean;
-  // isSmallHeader: boolean;
   isTypingAllowed: boolean;
-  // temp: string;
   internalSoulid: number;
 }
 
@@ -27,19 +15,7 @@ const internalSetting: IInternalData = {
   firstMessage: false,
   isHaveNewQuestion: "",
   userQuestionType: "intro",
-  // showCommonModal: false,
-  // showMenuModal: false,
-  // showLoginModal: false,
-  // showSignupModal: false,
-  // showAboutModal: false,
-  // showRestorePasswordModal: false,
-  // showPaywallModal: false,
-  // showPaywallPricingModal: false,
-  // showPaywallCardpayModal: false,
-  // showPaywallResultModal: false,
-  // isSmallHeader: false,
   isTypingAllowed: false,
-  // temp: "",
   internalSoulid: 0,
 };
 
@@ -91,81 +67,6 @@ export const internalSlice = createSlice({
       if (action.payload.message) state.dialog.push(action.payload.message);
     },
 
-    // showMenuModal(state) {
-    //   state.showCommonModal = !state.showCommonModal;
-    //   state.showMenuModal = !state.showMenuModal;
-    // },
-
-    // showReadAboutModal(state) {
-    //   state.showCommonModal = true;
-    //   state.showAboutModal = true;
-    // },
-
-    // backdropClick(state) {
-    //   state.showCommonModal = false;
-    //   state.showMenuModal = false;
-    //   state.isSmallHeader = false;
-    //   state.showLoginModal = false;
-    //   state.showSignupModal = false;
-    //   state.showAboutModal = false;
-    //   state.showPaywallModal = false;
-    //   state.showPaywallCardpayModal = false;
-    //   state.showPaywallPricingModal = false;
-    //   state.showPaywallResultModal = false;
-    //   state.showRestorePasswordModal = false;
-    // },
-
-    // toggleToLogin(state) {
-    //   state.showCommonModal = true;
-    //   state.showMenuModal = false;
-    //   state.showLoginModal = true;
-    //   state.isSmallHeader = true;
-    //   state.showSignupModal = false;
-    // },
-
-    // toggleToSignup(state) {
-    //   state.showCommonModal = true;
-    //   state.showMenuModal = false;
-    //   state.showLoginModal = false;
-    //   state.isSmallHeader = true;
-    //   state.showSignupModal = true;
-    // },
-
-    // toggleLoginSignup(state) {
-    //   state.showCommonModal = true;
-    //   state.showLoginModal = !state.showLoginModal;
-    //   state.showSignupModal = !state.showSignupModal;
-    // },
-
-    // toggleToAbout(state) {
-    //   state.showCommonModal = true;
-    //   state.showAboutModal = true;
-    //   state.isSmallHeader = true;
-    //   state.showMenuModal = false;
-    // },
-
-    // toggleToPayment(state) {
-    //   state.showPaywallModal = true;
-    //   state.showPaywallPricingModal = true;
-    // },
-
-    // toggleToCardpay(state) {
-    //   state.showPaywallModal = true;
-    //   state.showPaywallPricingModal = false;
-    //   state.showPaywallCardpayModal = true;
-    // },
-
-    // toggleToResult(state) {
-    //   state.showPaywallModal = true;
-    //   state.showPaywallCardpayModal = false;
-    //   state.showPaywallResultModal = true;
-    // },
-
-    // toggleLoginChangePassword(state) {
-    //   state.showLoginModal = !state.showLoginModal;
-    //   state.showRestorePasswordModal = !state.showRestorePasswordModal;
-    // },
-
     disallowTyping(state) {
       state.isTypingAllowed = false;
     },
@@ -173,14 +74,6 @@ export const internalSlice = createSlice({
     allowTyping(state) {
       state.isTypingAllowed = true;
     },
-
-    // setTemp(state, action: PayloadAction<{ message: string }>) {
-    //   state.temp = action.payload.message;
-    // },
-
-    // deleteTemp(state) {
-    //   state.temp = "";
-    // },
 
     setSoulId(state, action: PayloadAction<{ soulid: number }>) {
       state.internalSoulid = action.payload.soulid;
